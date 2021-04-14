9f19914dac5f40b19234df98717e7089f3f6c96d
Jacky Nguyen <nktpro@gmail.com>
2021-04-13 21:36:17 -0400

    Export * for typebox dependency

--- src/deps/typebox.ts
+++ src/deps/typebox.ts
@@ -1,9 +1,1 @@
-export {
-  Type,
-} from "https://raw.githubusercontent.com/shopstic/typebox/0.16.4/src/typebox.ts";
-export type {
-  Static,
-  TObject,
-  TProperties,
-  TSchema,
-} from "https://raw.githubusercontent.com/shopstic/typebox/0.16.4/src/typebox.ts";
+export * from "https://raw.githubusercontent.com/shopstic/typebox/0.16.4/src/typebox.ts";
