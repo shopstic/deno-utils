edb45b0ae81c95b53d1ff95cc27fe8a6fa2ecdf4
Jacky Nguyen <nktpro@gmail.com>
2021-11-15 23:16:57 -0500

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,17 @@
 {
   description = "Deno Utils";
 
-  inputs.flakeUtils.url = "github:numtide/flake-utils";
-  inputs.nixHotPot.url = "github:shopstic/nix-hot-pot";
+  inputs = {
+    flakeUtils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixHotPot = {
+      url = "github:shopstic/nix-hot-pot";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flakeUtils.follows = "flakeUtils";
+    };
+  };
 
   outputs = { self, nixpkgs, flakeUtils, nixHotPot }:
     flakeUtils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
