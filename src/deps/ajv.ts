52d6743cf92782902d3045fffbf0d4291040960b
Jacky Nguyen <nktpro@gmail.com>
2022-02-08 22:02:32 -0500

    Switch CDN

--- src/deps/ajv.ts
+++ src/deps/ajv.ts
@@ -1,11 +1,11 @@
-// @deno-types="https://cdn.skypack.dev/pin/ajv@v8.6.2-hTXOSR89efxJTuZb2Htb/dist=es2020,mode=types/index.d.ts"
+// @deno-types="https://cdn.shopstic.com/pin/ajv@v8.6.2-hTXOSR89efxJTuZb2Htb/dist=es2020,mode=types/index.d.ts"
 import Ajv, {
   ErrorObject,
   ErrorsTextOptions,
   Options,
-} from "https://cdn.skypack.dev/pin/ajv@v8.6.2-hTXOSR89efxJTuZb2Htb/dist=es2020,mode=imports/optimized/ajv.js";
-// @deno-types="https://cdn.skypack.dev/pin/ajv-formats@v2.1.1-vcFtNZ2SctUV93FmiL2Q/dist=es2020,mode=types/dist/index.d.ts"
-import addFormats from "https://cdn.skypack.dev/pin/ajv-formats@v2.1.1-vcFtNZ2SctUV93FmiL2Q/dist=es2020,mode=imports/optimized/ajv-formats.js";
+} from "https://cdn.shopstic.com/pin/ajv@v8.6.2-hTXOSR89efxJTuZb2Htb/dist=es2020,mode=imports/optimized/ajv.js";
+// @deno-types="https://cdn.shopstic.com/pin/ajv-formats@v2.1.1-vcFtNZ2SctUV93FmiL2Q/dist=es2020,mode=types/dist/index.d.ts"
+import addFormats from "https://cdn.shopstic.com/pin/ajv-formats@v2.1.1-vcFtNZ2SctUV93FmiL2Q/dist=es2020,mode=imports/optimized/ajv-formats.js";
 
 export { addFormats, Ajv };
 export type { ErrorObject, ErrorsTextOptions, Options };
