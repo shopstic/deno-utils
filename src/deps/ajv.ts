5bd2cda7eb5302d55a83add38c9e64c14c7bab3b
Jacky Nguyen <nktpro@gmail.com>
2023-05-01 11:50:34 -0400

    Update dependencies

--- src/deps/ajv.ts
+++ src/deps/ajv.ts
@@ -1,5 +1,6 @@
-import Ajv, { ErrorObject, ErrorsTextOptions, Options } from "https://esm.sh/ajv@8.12.0?pin=v106";
-import addFormats from "https://esm.sh/ajv-formats@2.1.1?pin=v106";
+import Ajv from "https://esm.sh/ajv@8.12.0?pin=v118";
+import type { ErrorObject, ErrorsTextOptions, Options } from "https://esm.sh/ajv@8.12.0?pin=v118";
+import addFormats from "https://esm.sh/ajv-formats@2.1.1?pin=v118";
 
 export { addFormats, Ajv };
 export type { ErrorObject, ErrorsTextOptions, Options };
